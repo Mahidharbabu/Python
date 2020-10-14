dadb1b9c2cdcf2b96258f0879998a5ec4024875f
Mahidharbabu <mahidhar0607@gmail.com>
2020-10-14 14:19:11 +0530

    syntax issues fixed

--- Simple Functions.py
+++ Simple Functions.py
@@ -1,23 +1,33 @@
-def add(a,b):
-    return a+b
-def sub(a,b):
-    return a-b
-def mul(a,b):
-    return a*b
-def div(a,b):
-    return a/b
-def rem(a,b):
-    return a%b
-def pwr(a,b):
-    return pow(a,b)
+def add(var1, var2):
+    return var1+var2
+
+
+def sub(var1, var2):
+    return var1-var2
+
+
+def mul(var1, var2):
+    return var1*var2
+
+
+def div(var1, var2):
+    return var1/var2
+
+
+def rem(var1, var2):
+    return var1 % var2
+
+
+def pwr(var1, var2):
+    return pow(var1, var2)
 
 
 a = int(input('Enter value of a: '))
 b = int(input('Enter value of b: '))
-print('sum of A and B is ',add(a,b))
-print('sub of A and B is ',sub(a,b))
-print('mul of A and B is ',mul(a,b))
-print('div of A and B is ',div(a,b))
-print('rem of A and B is ',rem(a,b))
-print('pwr of A and B is ',pwr(a,b))
+print('sum of A and B is ', add(a, b))
+print('sub of A and B is ', sub(a, b))
+print('mul of A and B is ', mul(a, b))
+print('div of A and B is ', div(a, b))
+print('rem of A and B is ', rem(a, b))
+print('pwr of A and B is ', pwr(a, b))
 print('I would like to add few more functions')
