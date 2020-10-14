0b20b4d5e35f32864e0b3af8e17136d754b9b142
Mahidharbabu <mahidhar0607@gmail.com>
2020-10-14 13:54:04 +0530

    Added few more functions

--- Simple Functions.py
+++ Simple Functions.py
@@ -12,11 +12,12 @@ def pwr(a,b):
     return pow(a,b)
 
 
-a = int(input('Enter value of a'))
-b = int(input('Enter value of b'))
+a = int(input('Enter value of a: '))
+b = int(input('Enter value of b: '))
 print('sum of A and B is ',add(a,b))
 print('sub of A and B is ',sub(a,b))
 print('mul of A and B is ',mul(a,b))
 print('div of A and B is ',div(a,b))
 print('rem of A and B is ',rem(a,b))
 print('pwr of A and B is ',pwr(a,b))
+print('I would like to add few more functions')
