fde856e73e9a543e6d7d5d6e5e0e9d9feed5f5d2
Mahidharbabu <mahidhar0607@gmail.com>
2020-10-14 13:45:31 +0530

    Added few more functions

--- Simple Functions.py
+++ Simple Functions.py
@@ -1,4 +1,22 @@
 def add(a,b):
     return a+b
+def sub(a,b):
+    return a-b
+def mul(a,b):
+    return a*b
+def div(a,b):
+    return a/b
+def rem(a,b):
+    return a%b
+def pwr(a,b):
+    return pow(a,b)
 
-print('sum of 5 and 8 is ',add(5,8))
+
+a = int(input('Enter value of a'))
+b = int(input('Enter value of b'))
+print('sum of A and B is ',add(a,b))
+print('sub of A and B is ',sub(a,b))
+print('mul of A and B is ',mul(a,b))
+print('div of A and B is ',div(a,b))
+print('rem of A and B is ',rem(a,b))
+print('pwr of A and B is ',pwr(a,b))
